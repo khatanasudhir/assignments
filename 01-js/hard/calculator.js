1e77f396e4147029922e0b998990098a7f38fef7
khatanasudhir <khatanasudhir786@gmail.com>
2023-12-20 16:57:05 +0530

    Assignment Solutions

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,57 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(number) {
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.result *= number;
+  }
+
+  divide(number) {
+    if (number === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    const sanitizedExpression = expression.replace(/\s+/g, ''); // Remove continuous spaces
+    const validCharacters = /^[0-9+\-*/(). ]+$/;
+
+    if (!validCharacters.test(sanitizedExpression)) {
+      throw new Error("Invalid characters in the expression");
+    }
+     // Check for division by zero explicitly
+    if (sanitizedExpression.includes('/0')) {
+      throw new Error("Cannot divide by zero");
+    }
+
+    try {
+      this.result = eval(sanitizedExpression);
+    } catch (error) {
+      throw new Error("Invalid expression");
+    }
+  }
+}
 
 module.exports = Calculator;
+
